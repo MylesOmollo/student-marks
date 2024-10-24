0441527432fff67b7309b3b6e81725acf0d3fb20
MylesOmollo <159228636+MylesOmollo@users.noreply.github.com>
2024-10-24 11:04:03 +0300

    Add files via upload

---
+++ studentmarks.java
@@ -0,0 +1,27 @@
+class StudentMarks {
+
+    public static void main(String[]args){
+        Int AverageMarks =50;
+        if (AverageMarks >= 50) {
+            System.out.println("Passed");
+         }else{
+            System.out.println("Failed");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
